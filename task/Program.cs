98a11185324172c58791536cc50a9e263e255db9
ivan <ivan@vn.com>
2022-08-24 11:48:45 +0300

    add first method

---
+++ task/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Условие задачи:
+
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа.
+*/
+
+int FindSize(string[] arbitraryArray, int requiredSize)             // Метод определения длинны итогового массива.
+{
+    int size = 0;
+    for (int i = 0; i < arbitraryArray.Length; i++)
+    {
+        if (arbitraryArray[i].Length <= requiredSize) size++;
+    }
+    return size;
+}
+
